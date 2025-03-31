174338d24931a67c7a4a3f7d823a06f7fe2a875d
juldgvn <digiovannijulien@gmail.com>
2025-03-31 21:14:11 +0200

    init

---
+++ app/__generated__/[untitled]._index.tsx
@@ -0,0 +1,33 @@
+/* eslint-disable */
+      /* This is a auto generated file for building the project */ 
+
+
+      import { Fragment, useState } from "react";
+      import { useResource, useVariableState } from "@webstudio-is/react-sdk/runtime";
+      import { Body as Body } from "@webstudio-is/sdk-components-react-router";
+
+
+      export const siteName = undefined;
+
+      export const breakpoints = [{"id":"LwIMnGYV7KvvzdWGa1ngV"},{"id":"xkhW6I0DFXfI0c-04fLQq","maxWidth":991},{"id":"ToS_uK19UHrXy490cLtsj","maxWidth":767},{"id":"mq9fI1cKdlsYFjls8G_Ci","maxWidth":479}];
+
+      export const favIconAsset: string | undefined =
+        undefined;
+
+      // Font assets on current page (can be preloaded)
+      export const pageFontAssets: string[] =
+        []
+
+      export const pageBackgroundImageAssets: string[] =
+        []
+
+      
+
+      const Page = (_props: { system: any; }) => {
+return <Body
+className={`w-body`} />
+}
+
+
+      export { Page }
+    
---
+++ app/__generated__/_index.tsx
@@ -0,0 +1,48 @@
+/* eslint-disable */
+      /* This is a auto generated file for building the project */ 
+
+
+      import { Fragment, useState } from "react";
+      import { useResource, useVariableState } from "@webstudio-is/react-sdk/runtime";
+      import { Body as Body } from "@webstudio-is/sdk-components-react-router";
+import { Box as Box, Heading as Heading } from "@webstudio-is/sdk-components-react";
+
+
+      export const siteName = undefined;
+
+      export const breakpoints = [{"id":"LwIMnGYV7KvvzdWGa1ngV"},{"id":"xkhW6I0DFXfI0c-04fLQq","maxWidth":991},{"id":"ToS_uK19UHrXy490cLtsj","maxWidth":767},{"id":"mq9fI1cKdlsYFjls8G_Ci","maxWidth":479}];
+
+      export const favIconAsset: string | undefined =
+        undefined;
+
+      // Font assets on current page (can be preloaded)
+      export const pageFontAssets: string[] =
+        []
+
+      export const pageBackgroundImageAssets: string[] =
+        []
+
+      
+            
+
+            export const CustomCode = () => {
+              return (<></>);
+            }
+          
+
+      const Page = (_props: { system: any; }) => {
+return <Body
+className={`w-body`}>
+<Box
+className={`w-box c1h321yh cerqx3f`}>
+<Heading
+className={`w-heading cx9xznx`}>
+{"Hello World"}
+</Heading>
+</Box>
+</Body>
+}
+
+
+      export { Page }
+    
